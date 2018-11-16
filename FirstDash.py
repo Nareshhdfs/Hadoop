6e3272f7a09033465f94c0904c5271c34cf2b36d
Nareshhdfs <naresh.hdfs@gmail.com>
2018-11-16 13:38:43 -0500

    Add files via upload

---
+++ FirstDash.py
@@ -0,0 +1,40 @@
+﻿# -*- coding: utf-8 -*-
+"""
+Created on Fri May 11 09:52:42 2018
+
+@author: Bokkisam
+"""
+
+import dash
+import dash_core_components as dcc
+
+import dash_html_components as html
+
+app = dash.Dash()
+
+colors={'background':"",
+        'text':''}
+
+app.layout = html.Div(children=[
+    html.H1(children='Hello Dash'),
+
+    html.Div(children='''
+        Dash: A web application framework for Python.
+    '''),
+
+    dcc.Graph(
+        id='example-graph',
+        figure={
+            'data': [
+                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
+                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
+            ],
+            'layout': {
+                'title': 'Dash Data Visualization'
+            }
+        }
+    )
+])
+
+if __name__ == '__main__':
+    app.run_server(port=8050)
